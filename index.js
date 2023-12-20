0de9719b411db5c388782f51c0a1299a192d820b
student <mdshishov@mail.ru>
2023-12-20 10:23:49 +0300

    Counter added, Linter checked

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const whoWins = (player1, player2) => {
-  if (player1 === player2) {
-    return 0;
-  }
-
   if (player1 === 1) {
     if (player2 === 2) {
       return 1;
@@ -31,71 +27,89 @@ const whoWins = (player1, player2) => {
       return 2;
     }
   }
+
+  return 0;
 };
 
 const resultMessage = (user, computer) => {
-  const lines = ['Камень ломает ножницы', 'Ножницы режут бумагу', 'Бумага оборачивает камень'];
+  const lines = ['Камень ломает ножницы.', 'Ножницы режут бумагу.', 'Бумага оборачивает камень.'];
   const winner = whoWins(user, computer);
-  let result = '';
+  let message = '';
 
   if (winner === 1) {
-    result += 'Вы победили! ';
+    message += 'Вы победили! ';
   } else if (winner === 2) {
-    result += 'Победил компьютер. ';
+    message += 'Победил компьютер. ';
   } else {
-    return 'Ничья.';
+    return ['Ничья.', winner];
   }
 
   if ((user === 1 || user === 2) && (computer === 1 || computer === 2)) {
-    return `${result}${lines[0]}.`;
+    message += lines[0];
   }
   if ((user === 2 || user === 3) && (computer === 2 || computer === 3)) {
-    return `${result}${lines[1]}.`;
+    message += lines[1];
   }
   if ((user === 1 || user === 3) && (computer === 1 || computer === 3)) {
-    return `${result}${lines[2]}.`;
+    message += lines[2];
   }
+
+  return [message, winner];
 };
 
-const getRandom = () => Math.floor(Math.random() * 2 + 1);
+const getRandom = (n = 3, m = 1) => Math.floor(Math.random() * n + m);
 
-const gameStage = () => {
+const gameRound = () => {
   console.log('\nВыберите вашу фигуру:');
-  
+
   const moves = ['Камень', 'Ножницы', 'Бумага'];
   for (let i = 0; i < moves.length; i += 1) {
     console.log(`${i + 1}. ${moves[i]}`);
   }
 
-  console.log();
-  const index = Number(readlineSync.keyIn('Ваш выбор: ', {limit: '$<1-3>'}));
-  console.log(`Ваш выбор: ${moves[index - 1]}`);
+  const user = Number(readlineSync.keyIn('\nВаш выбор: ', { limit: '$<1-3>' }));
+  console.log(`Ваш выбор: ${moves[user - 1]}\n`);
 
   const computer = getRandom();
-  console.log(`\nКомпьютер выбирает: ${moves[computer - 1]}`);
+  console.log(`Компьютер выбирает: ${moves[computer - 1]}\n`);
 
-  console.log('\nРезультат: ' + resultMessage(index, computer));
+  const [message, winner] = resultMessage(user, computer);
+  console.log(`Результат: ${message}\n`);
+
+  return winner;
 };
 
 const isEnd = () => {
-  const ask = readlineSync.question('\nХотите сыграть ещё раз? (да/нет): ', {
+  const ask = readlineSync.question('Хотите сыграть ещё раз? (да/нет): ', {
     limit: ['да', 'нет'],
-    limitMessage: 'Введите, пожалуйста "да" или "нет".'
+    limitMessage: 'Введите, пожалуйста "да" или "нет".',
   });
 
   return ask === 'да';
-}
+};
 
 const game = () => {
   console.log('Добро пожаловать в игру "Камень, Ножницы, Бумага"!');
-  let end = true;
 
+  let userCount = 0;
+  let computerCount = 0;
+
+  let flag = true;
   do {
-    gameStage();
-    end = isEnd();
-  } while (end);
+    const winner = gameRound();
 
-  console.log('\nСпасибо за игру! До встречи!')
+    if (winner === 1) {
+      userCount += 1;
+    }
+    if (winner === 2) {
+      computerCount += 1;
+    }
+    console.log(`Пользователь ${userCount} | ${computerCount} Компьютер`);
+
+    flag = isEnd();
+  } while (flag);
+
+  console.log('\nСпасибо за игру! До встречи!');
 };
 
 game();
